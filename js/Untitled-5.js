5b34cf15df181c0459ebc45cb9d077380573a0fc
zx2625425740 <2625425740@qq.com>
2017-08-20 16:04:09 +0800

    zhangxu6

---
+++ js/Untitled-5.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+$(function() {
+	var bannerSlider = new Slider($('#banner_tabs'), {
+		time: 5000,
+		delay: 400,
+		event: 'hover',
+		auto: true,
+		mode: 'fade',
+		controller: $('#bannerCtrl'),
+		activeControllerCls: 'active'
+	});
+	$('#banner_tabs .flex-prev').click(function() {
+		bannerSlider.prev()
+	});
+	$('#banner_tabs .flex-next').click(function() {
+		bannerSlider.next()
+	});
+})
+
